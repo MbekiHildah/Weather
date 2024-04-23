404369d5b452e9c0ef65a169c38c930a71e374f7
MbekiHildah <mbekihildah@gmail.com>
2024-04-23 23:37:51 +0100

    updated forecast API

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ function refreshWeather(response) {
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
+
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -51,20 +53,39 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+function getForecast(city) {
+  let apiKey = "2ft2755f62423283e36ab5068a024o8d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day"><div class="day">${day}</div>
-          <div class="forecast-widget">⛈</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day"><div class="day">${formatDate(
+          day.time
+        )}</div>
+
+  <img src="${day.condition.icon_url}" class="forecast-widget"/>
           <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">10° </span>
-            <span class="weather-forecast-temperature-min">6°</span>
+            <span class="weather-forecast-temperature-max">${Math.round(
+              day.temperature.maximum
+            )}° </span> 
+            <span class="weather-forecast-temperature-min"> ${Math.round(
+              day.temperature.minimum
+            )}°</span>
           </div>
           </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -75,4 +96,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Celbridge");
-displayForecast();
